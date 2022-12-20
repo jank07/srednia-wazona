fd5607f0e8239a248d551934841da26782bb8645
jankorczak <113556340+jankorczak@users.noreply.github.com>
2022-12-20 11:25:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 let weight = document.querySelector('.weight')
     document.querySelector('.submitNumberWeight').onclick = function() { 
-    let string = document.querySelector('#numberWeight').value;
-    let numbers = [];
-    if (string == null || string.trim().length == 0) alert('error')
+        let string = document.querySelector('#numberWeight').value;
+        let numbers = [];
+        if (string == null || string.trim().length == 0) {
+            alert('error')
+        }
 
-    else {
-        string = string.split(',')
-        for (let i = 0; i < string.length; i++) {
-        if (parseInt(string[i]) <= 10 && parseInt(string[i]) >= 1) {
-        numbers.push(string[i])
-        
+        else {
+            string = string.split(',')
+            for (let i = 0; i < string.length; i++) {
+                string[i] = parseInt(string[i])
+                if (string[i] <= 10 && string[i] >= 1) {
+                    numbers.push(string[i])
+            
+        }
+        }
+        console.log(numbers)
+        numbers.forEach(num => {
+            console.log(num);
+            num = parseInt(num);
+            weight.at(num).style.display = 'block';
+        })
+        const arr2 = [1, 2, 3]
+        arr2.forEach (el => {
+
+        })
     }
 }
-    console.log(numbers)
-    numbers.forEach(el => {
-        weight[el].style.display = 'block';
-    })
-}
-}
+
+
+    
