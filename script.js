ee57c8929510d6b0771658f394150c3e242d0222
jankorczak <113556340+jankorczak@users.noreply.github.com>
2022-12-24 17:50:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
-document.querySelector('.submitNumberWeight').onclick = function() { 
-        let weight = document.querySelectorAll('.weight');
-        // let weights = {}; 
-        // for(let i = 1; i <= 10; i++) {
-        //     weights["weight" + i] = document.querySelector('.weight' + i)
-        // }
+const weightsEl = document.querySelector('.weights');
+const weightEl = weightsEl.querySelector('.weight');
+const templateRef = weightEl.cloneNode(true);
+weightEl.remove();
 
-        let string = document.querySelector('#numberWeight').value;
-        let numbers = [];
-        if (string == null || string.trim().length == 0) {
-            alert('error')
-        }
-
-        else {
-            string = string.split(',')
-            for (let i = 0; i < string.length; i++) {
-                string[i] = parseInt(string[i])
-                if (string[i] <= 10 && string[i] >= 1) {
-                    numbers.push(string[i])
-            
-        }
-        }
-        console.log(numbers)
-        numbers.forEach(num => {
-            console.log(num);
-            num = parseInt(num);
-            // weight[num].style.display = 'block';
-            console.log(weights[num]);
-        })
-        
-    }
+const weightsInput = document.querySelector('#numberWeight');
+const weightsSubmitBtn = document.querySelector('.submitNumberWeight');
+if (!weightsSubmitBtn) {
+    throw new Error('Element not found');
 }
 
+const submitEl = document.querySelector('.submitNumber');
 
+const explodeStringAsNumers = (input) => input.value.trim()
+    .split(',')
+    .map((numAsString) => parseInt(numAsString.trim(), 10));
+
+weightsSubmitBtn.addEventListener('click', function() {
+    const weights = explodeStringAsNumers(weightsInput).filter((num, i, arr) => arr.indexOf(num, i + 1) === -1);
     
+    if (!weights.length) {
+        return alert('No weights specified');
+    }
+    
+    if (weights.some((weight) => weight <= 0 || weight > 10)) {
+        return alert('Wagi od 1 do 10!');
+    }
+    
+    const map = weights.map((weight, index) => {
+        const template = templateRef.cloneNode(true);
+        const id = `weight-${index}`;
+
+        const label = template.querySelector('label');
+        label.for = id;
+        label.textContent = `Podaj swoją ocenę z wagą ${weight}: `;
+
+        const input = template.querySelector('input');
+        input.id = id;
+        weightsEl.appendChild(template);
+
+        return [weight, input];
+    });
+
+    submitEl.addEventListener('click', () => {
+        let weightSum = 0;
+        const gradeSum = map.reduce((sum, [ weight, input ]) => {
+            weightSum += weight;
+            return sum + explodeStringAsNumers(input).reduce((sum2, grade) => sum2 + grade * weight, 0);
+        }, 0);
+
+        alert(gradeSum / weightSum);
+    });
+});
+
+
+
